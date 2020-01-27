5ffccc21c0e13e108d3cc03d0a1febdececbc064
Jung Yu <hjyu1408@gmail.com>
2020-01-27 02:20:26 -0500

    Make login page with Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 
 
 app = Flask(__name__)
@@ -13,6 +13,8 @@ migrate = Migrate(app,db)
 app.static_folder = 'static'
 login = LoginManager(app)
 login.login_view = 'login' # @login_required
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,61 @@
-from app import app
+from flask import (
+    redirect,
+    render_template,
+    request,
+    flash,
+    url_for
+    )
+from app import app, db
+from app.models import Usage, User, Receipt
 from flask_login import current_user, login_user, logout_user, login_required
+from app.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_POST():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    user = User.query.filter_by(username=username).first()
+
+    if user is None:
+        flash("There is no username [{}]".format(username))
+        return redirect('/login')
+
+    if not user.check_password(password):
+        flash("Password is incorrect")
+        return redirect('/login')
+
+    login_user(user)
+    return redirect('/todo')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(username= form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congrat! you are now a new user of blog')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
